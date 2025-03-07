a2bc979296c44f617e0c2487ab8d38b1b50c792f
NgHoaiTan <tannh3108@gmail.com>
2025-03-07 22:41:33 +0700

    get profile and edit profile

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,6 +2,8 @@ import { User, User } from '../models/user.model';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import 'dotenv/config';
+import getDataUri from '../config/datauri';
+import cloudinary from '../config/cloudinary';
 
 export const register = async (req, res) => {
     try {
@@ -81,4 +83,55 @@ export const logout = async (req, res) => {
     } catch (error) {
         console.log(error);
     }
-} 
+}
+
+export const getProfile = async (req, res) => {
+    try {
+        const userId = req.params.id;
+        let user = await User.findById(userId);
+        return res.status(200).json({
+            user,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const editProfile = async (req, res) => {
+    try {
+        const userId = req.id;
+        const { bio, gender } = req.body;
+        const profilePicture = req.file;
+        let cloudResponse;
+
+        if (profilePicture) {
+            const fileUri = getDataUri(profilePicture);
+            cloudResponse = await cloudinary.uploader.upload(fileUri);
+        }
+
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({
+                message: 'User not found',
+                success: false
+            })
+        };
+        if (bio) {
+            user.bio = bio;
+        }
+        if (gender) user.gender = gender;
+        if (profilePicture) user.profilePicture = cloudResponse.secure_url;
+
+        await user.save();
+
+        return res.status(200).json({
+            message: 'Profile updated',
+            success: true,
+            user
+        })
+    } catch (error) {
+        console.log(error);
+    }
+};
+
