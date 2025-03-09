6408a16d5a19ec27c0bda29e5908f6496a1e41ca
NgHoaiTan <tannh3108@gmail.com>
2025-03-09 17:50:58 +0700

    message controller

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,64 @@
+import Conversation from '../models/Conversation.js';
+import Message from '../models/Message.js';
+
+export const sendMessage = async (req, res) => {
+    try {
+        const senderId = req.id;
+        const receiverId = req.params.id;
+        const { message } = req.body;
+
+        let conversation = await Conversation.findOne({
+            participants: { $all: [senderId, receiverId] },
+        });
+
+        if (!conversation) {
+            conversation = await Conversation.create({
+                participants: [senderId, receiverId],
+            });
+        }
+        const newMessage = await Message.create({
+            sender: senderId,
+            receiver: receiverId,
+            message,
+        });
+
+        if (newMessage) {
+            conversation.message.push(newMessage._id);
+        }
+        await Promise.all([conversation.save(), newMessage.save()]);
+
+        // socket.io implementation for real time notification
+
+        return res.status(200).json({
+            success: true,
+            message: 'Message sent successfully',
+            newMessage
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getMessage = async (req, res) => {
+    try {
+        const senderId = req.id;
+        const receiverId = req.params.id;
+        const conversation = await Conversation.findOne({
+            participants: { $all: [senderId, receiverId] },
+        });
+        if (!conversation) {
+            return res.status(200).json({
+                success: true,
+                message: []
+            });
+        }
+        return res.status(200).json({
+            success: true,
+            message: conversation?.message
+        });
+
+    } catch (error) {
+        console.log(error);
+
+    }
+}
