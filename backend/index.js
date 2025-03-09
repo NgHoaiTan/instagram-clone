224951227c2b395e6d5be18555138f6f23d191c4
NgHoaiTan <tannh3108@gmail.com>
2025-03-10 00:50:05 +0700

    SignUp page

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
     origin: 'http://localhost:5173',
-    credential: true
+    credentials: true
 }
 app.use(cors(corsOptions));
 
